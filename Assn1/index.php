39d43e9bc43f94eaca450fc641c7c5ce7b9c70b6
David Merrick <david@david-merrick.com>
2014-01-16 13:26:09 -0800

    first commit

--- Assn1/index.php
+++ Assn1/index.php
@@ -1,1 +1,44 @@
-Hello World
+<html>
+	<head>
+		<title>David's Web App</title>
+		<meta name="description" content="Awesome Description Here"> <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="jquery.mobile-1.4.0/jquery.mobile-1.4.0.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$(".redirectbutton").click(function(){
+					window.location.href = "http://www.google.com";
+				});
+			});
+		</script>
+	</head>
+	<body>
+		<div id="heading">
+			<h1>Lorem ipsum</h1>
+		</div>
+		<div id="buttons">
+			<form>
+				<input type="button" class="redirectbutton" value="Button 1">
+				<input type="button" class="redirectbutton" value="Button 2">
+				<input type="button" class="redirectbutton" value="Button 3">
+			</form>
+		</div>
+		<div id="ulist">
+			<ul>
+				<li>Dolor sit amet, cu usu volutpat partiendo scripserit.</li>
+				<li>Quo tantas petentium ne.</li>
+				<li>Eos adipisci oportere reprimique eu.</li>
+			</ul>
+		</div>
+		<div id="story">
+			<div class="story-title">
+				<h2>Per mentitum salutandi appellantur in</h2>
+			</div>
+			<div class="story-body">
+				At utinam eligendi voluptaria eum, cum an errem facete pertinax, no quo dictas dicunt. Eum ea fastidii tacimates, in mel debitis accusam signiferumque. Veritus mnesarchum qui eu.Ullum exerci ne cum. Eos quas vulputate conclusionemque in, ius assum eirmod labores te. 
+			</div>
+		</div>
+	</body>
+</html>
